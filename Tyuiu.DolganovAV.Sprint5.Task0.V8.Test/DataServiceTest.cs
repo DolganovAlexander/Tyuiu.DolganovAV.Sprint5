7797d92b4d68ca268a6e2389cc141948fa3e5b75
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-24 17:33:41 +0500

    Add project files.

---
+++ Tyuiu.DolganovAV.Sprint5.Task0.V8.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.DolganovAV.Sprint5.Task0.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckeedExistsFile()
+        {
+            string path = @"C:\Users\canya\AppData\Local\Temp\OutPutFileTask0.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool fileExists = fileinfo.Exists;
+            Assert.IsTrue(fileExists);
+        }
+    }
+}
