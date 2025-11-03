8a15693d46ace230183bf97937155d6d6bdfc4bc
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-11-03 16:08:03 +0500

    Task3

---
+++ Tyuiu.DolganovAV.Sprint5.Task3.V29.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿namespace Tyuiu.DolganovAV.Sprint5.Task3.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void SaveToFileTextData()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo FileInfo = new FileInfo(path);
+            bool FileExist = File.Exists(path);
+            Assert.AreEqual(true, FileExist);
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint5.Task3.V29/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.DolganovAV.Sprint5.Task3.V29.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File                                                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение и записывает результат  *");
+        Console.WriteLine("* в бинарный файл                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 3;
+        Console.WriteLine($"x = {x}");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = ds.SaveToFileTextData(x);
+        Console.WriteLine($"Файл: {path}");
+        Console.WriteLine("Создан");
+        Console.ReadKey();
+
+    }
+}
