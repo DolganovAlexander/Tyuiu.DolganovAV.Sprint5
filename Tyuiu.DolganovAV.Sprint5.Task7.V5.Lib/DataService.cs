48fc093bbe156ff4a3c26eb099d307477bbb5602
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-11-26 01:18:39 +0500

    Task7

--- Tyuiu.DolganovAV.Sprint5.Task7.V5.Lib/DataService.cs
+++ Tyuiu.DolganovAV.Sprint5.Task7.V5.Lib/DataService.cs
@@ -9,7 +9,8 @@ namespace Tyuiu.DolganovAV.Sprint5.Task7.V5.Lib
             string output = Path.Combine(Path.GetTempPath(), "OutPutDataFileTask7V5.txt");
             string content = File.ReadAllText(path);
             string res = Regex.Replace(content, "[a-zA-z]", "");
-            res = Regex.Replace(res, @"\s+", " ");
+            res = Regex.Replace(res, @"\s+([.,!?;:])", "$1");
+            res = Regex.Replace(res, @"\b \b", "");
             File.WriteAllText(output, res);
             return output;
         }
